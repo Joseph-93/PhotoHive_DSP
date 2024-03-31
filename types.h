56494fa7c7dce8e795831ebd5dd3598fb12d7435
joshuajosephmatheson <joshuajosephmatheson@gmail.com>
2024-03-31 17:42:58 -0600

    put into operable library format.

--- types.h
+++
@@ -1,9 +0,0 @@
-// types.h
-#ifndef TYPES_H
-#define TYPES_H
-
-typedef double Pixel;   // Official data type for pixels.
-
-// #define DEBUG   // Define debug to make all files turn to debug mode
-
-#endif // TYPES_H
